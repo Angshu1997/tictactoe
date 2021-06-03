23752cb5d68d604e0952367de2878b7af59ed4ac
Angshu1997 <38933142+Angshu1997@users.noreply.github.com>
2021-06-03 22:56:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+let gameActive=true;
+let currentPlayer="X";
+let gameState=["","","","","","","","",""];
+statusDisplay=document.querySelector(".game--status");
+
+
+
+function handleRestartGame(){
+	gameActive=true;
+	currentPlayer="X";
+	let gameState=["","","","","","","","",""];
+	document.querySelectorAll(".cell").forEach(cell => cell.innerText="");
+	statusDisplay.innerText=`It's ${currentPlayer}'s turn`;
+
+}
+
+function handleCellClick(clickedEvent){
+	const clickedCell=clickedEvent.target;
+	const clickedCellIndex=parseInt(clickedCell.getAttribute("data-cell-index"));
+	if (gameState[clickedCellIndex] != "" || !gameActive) {
+	 	return;
+	 }
+	 handleCellPlayer(clickedCell,clickedCellIndex);
+	 handleResultValidation();
+
+
+
+
+
+}
+
+function handleCellPlayer(clickedCell,clickedCellIndex){
+    gameState[clickedCellIndex]=currentPlayer;
+    clickedCell.innerText=currentPlayer;
+}
+
+const winningConditions=[
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+];
+
+function handleResultValidation(){
+	let roundWon=false;
+	for (let i = 0; i <=7; i++) {
+		const winCondition=winningConditions[i];
+		let a=gameState[winCondition[0]];
+		let b=gameState[winCondition[1]];
+		let c=gameState[winCondition[2]];	
+
+		if (a===""|| b===""||c==="") {
+			continue;
+		}
+
+		if (a==b && b==c) {
+			roundWon=true;
+			break;
+		}
+	}
+
+	if (roundWon) {
+		statusDisplay.innerText=`Player ${currentPlayer} has won`;
+		gameActive=false;
+		return;
+	}
+
+	let roundDraw=!gameState.includes("");
+	if (roundDraw) {
+		statusDisplay.innerText="It's a draw";
+		gameActive=false;
+		return;
+	}
+
+
+
+
+
+	handlePlayerChange();
+}
+
+function handlePlayerChange() {
+	// if (currentPlayer==="X") {
+	// 	currentPlayer="O";
+	// }
+	// else{
+	// 	currentPlayer="X";
+	// }
+    // doing this in one line
+    currentPlayer=currentPlayer==="X"?"O":"X";
+    statusDisplay.innerText=`It's ${currentPlayer}'s turn`;
+}
+document.querySelector(".game--restart").addEventListener("click",handleRestartGame);
+document.querySelectorAll(".cell").forEach(cell => cell.addEventListener("click",handleCellClick));
+
+
+
